db77e262107afff62e49d41243a24d9e388376f1
WILLIAM BOUET <william.bouet.14@gmail.com>
2022-11-07 17:09:35 +0100

    added Speedometer class

---
+++ Speedometer.php
@@ -0,0 +1,22 @@
+<?php
+class Speedometer
+{
+    public const ONE_MILE_IN_KM = 0.621;
+
+    public static function convertKmToMiles(int $km): float
+    {
+        return $km * self::ONE_MILE_IN_KM;
+    }
+
+    public static function convertMilesToKm(int $miles): float
+    {
+        return $miles / self::ONE_MILE_IN_KM;
+    }
+
+}
+
+
+echo Speedometer::convertKmToMiles(10);
+
+echo Speedometer::convertMilesToKm(100);
+
